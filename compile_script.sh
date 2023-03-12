8d999d26b905c82bd28d189a44a9d4ff8a686a44
Its-me-Andreeei <andrei.karolyi@student.upt.ro>
2023-03-12 12:34:23 +0200

    [SCRIPT] Updated compile script : bug fixes
    [SCRIPT] Updated compile script : bug fixes

--- compile_script.sh
+++ compile_script.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 clear
+
 gcc -Wall -c ./"$1"/process_management/process_management.c
 gcc -Wall -c ./"$1"/c_file_utils/c_files_utils.c
 gcc -Wall -c ./"$1"/directory_utils/directory_utils.c
+gcc -Wall -c ./"$1"/general_utils/general_utils.c
 gcc -Wall -c ./"$1"/main.c
 
-gcc -Wall -o exec main.o process_management.o directory_utils.o c_files_utils.o
+
+
+gcc -Wall -o macOS_executable main.o process_management.o directory_utils.o c_files_utils.o general_utils.o
 
 rm main.o
 rm process_management.o
 rm directory_utils.o
 rm c_files_utils.o
+rm general_utils.o
